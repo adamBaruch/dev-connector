b7455dc1b0e46e9c393f8c53f9dfa791bcc115f5
adam <shembeku@gmail.com>
2021-08-24 02:32:10 +0300

    first commit

---
+++ client/src/components/profile-forms/addEducation.js
@@ -0,0 +1,121 @@
+import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addEducation } from '../../actions/profile';
+
+const AddEducation = ({ addEducation, history }) => {
+  const [formData, setFromData] = useState({
+    school: '',
+    degree: '',
+    fieldofstudy: '',
+    from: '',
+    to: '',
+    current: false,
+    description: '',
+  });
+
+  const [toDateDisabled, setToDateDisabled] = useState(false);
+
+  const { school, degree, fieldofstudy, from, to, current, description } =
+    formData;
+
+  const onChange = (e) =>
+    setFromData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addEducation(formData, history);
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Add An Experience</h1>
+      <p className="lead">
+        <i className="fas fa-code-branch"></i> Add any developer/programming
+        positions that you have had in the past
+      </p>
+      <small>* = required field</small>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* School or Bootcamp"
+            name="school"
+            required
+            value={school}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Degree or Certificate"
+            name="degree"
+            required
+            value={degree}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Field of Study"
+            name="fieldofstudy"
+            value={fieldofstudy}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <h4>From Date</h4>
+          <input type="date" name="from" value={from} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <p>
+            <input
+              type="checkbox"
+              name="current"
+              checked={current}
+              value={current}
+              onChange={() => {
+                setFromData({ ...formData, current: !current });
+                setToDateDisabled(!toDateDisabled);
+              }}
+            />{' '}
+            Current School
+          </p>
+        </div>
+        <div className="form-group">
+          <h4>To Date</h4>
+          <input
+            type="date"
+            name="to"
+            value={to}
+            onChange={onChange}
+            disabled={toDateDisabled ? 'disabled' : ''}
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            cols="30"
+            rows="5"
+            placeholder="Program Description"
+            value={description}
+            onChange={onChange}
+          ></textarea>
+        </div>
+        <input type="submit" className="btn btn-primary my-1" />
+        <Link className="btn btn-light my-1" href="/dashboard">
+          Go Back
+        </Link>
+      </form>
+    </Fragment>
+  );
+};
+
+AddEducation.propTypes = {
+  addEducation: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addEducation })(withRouter(AddEducation));
